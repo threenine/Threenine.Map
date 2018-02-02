b907b502658bcc7282284b6594310b1eec296eb4
Gary Woodfine <gary.woodfine@threenine.co.uk>
2018-02-02 14:24:13 +0000

    configuring CI/CD

---
+++ UnitTests/DomainObjects/ComplexDomainObject.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using EntityObjects;
+using Threenine.Map;
+
+namespace DomainObjects
+{
+    public class ComplexDomainObject  :ICustomMap
+    {
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public string Summary { get; set; }
+        public string Title { get; set; }
+        public int Age { get; set; }
+        public void CustomMap(IMapperConfigurationExpression configuration)
+        {
+            configuration.CreateMap<ComplexDomainObject, SimpleEntity>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => string.Concat( src.Firstname, " ", src.LastName )))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => string.Concat( src.Title , " ", src.Summary)))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.Age));
+        }
+    }
+}
