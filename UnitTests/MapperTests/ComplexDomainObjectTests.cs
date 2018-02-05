cab9bfa9a3882f56efcff3bfc0e5f949091d5272
Gary Woodfine <gary.woodfine@threenine.co.uk>
2018-02-05 09:15:26 +0000

    adding missing files

---
+++ UnitTests/MapperTests/ComplexDomainObjectTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using DomainObjects;
+using EntityObjects;
+using FizzWare.NBuilder;
+using Threenine.Map;
+using Xunit;
+
+namespace MapperTests
+{
+    public class ComplexDomainObjectTests : IClassFixture<MapperFixture>, IDisposable
+    {
+        private readonly MapperFixture fixture;
+        public ComplexDomainObjectTests(MapperFixture fix)
+        {
+            fixture = fix;
+         
+        }
+        [Fact]
+        public void ShouldMapComplexObject()
+        {
+            
+            var testobj = Builder<ComplexDomainObject>.CreateNew().Build();
+            var ent = Mapper.Map<SimpleEntity>(testobj);
+            
+            Assert.NotNull(ent);
+            Assert.IsAssignableFrom<SimpleEntity>(ent);
+            Assert.Equal(ent.Name, string.Concat(testobj.Firstname, " ", testobj.LastName));
+            Assert.Equal(ent.Description, string.Concat(testobj.Title, " ", testobj.Summary));
+        }
+
+        [Fact]
+        public void ShouldMapSimpleObject()
+        {
+
+            var testobj = Builder<SimpleDomainObject>.CreateNew().Build();
+
+            var ent = Mapper.Map<SimpleEntity>(testobj);
+
+            Assert.NotNull(ent);
+            Assert.IsAssignableFrom<SimpleEntity>(ent);
+            Assert.Equal(ent.Name, testobj.Name);
+        }
+
+        public void Dispose()
+        {
+            fixture?.Dispose();
+        }
+    }
+}
---
+++ UnitTests/MapperTests/MapperFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using Threenine.Map;
+using Xunit;
+
+namespace MapperTests
+{
+    public class MapperFixture : IDisposable
+    {
+        public MapperFixture()
+        {
+            MapConfigurationFactory.Scan<MapperTests.MapperFixture>();
+        }
+
+        public void Dispose()
+        {
+          
+            // Do "global" teardown here; Only called once.
+        }
+    }
+
+}
