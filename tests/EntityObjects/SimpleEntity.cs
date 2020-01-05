4c8713ed8751d7d4b9d55ad77e5395cd3752d66a
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-01-05 21:22:57 +0000

    tidy up before upgrade

---
+++ tests/EntityObjects/SimpleEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace EntityObjects
+{
+    public class SimpleEntity 
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Age { get; set; }
+    }
+}
